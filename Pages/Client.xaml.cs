a7fea1d4875f1db27a3ae6c29c571b4228ef77c2
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-05-10 20:38:23 +0700

    Добавлен фильтр

--- Pages/Client.xaml.cs
+++ Pages/Client.xaml.cs
@@ -22,6 +22,21 @@ namespace CarService_SteeringWheel.Pages
     public partial class Client : Page
     {
         private User _currUser = null;
+        private List<Product> products;
+        private List<Product> fullProducts;
+        private string[] sortList =
+        {
+            "Без сортировки",
+            "Стоимость по возрастанию",
+            "Стоимость по убыванию"
+        };
+        private string[] filterDiscountList =
+        {
+            "Все диапазоны",
+            "0%-9,99%",
+            "10%-14,99%",
+            "15% и более"
+        };
 
         public Client()
         {
@@ -29,11 +44,20 @@ namespace CarService_SteeringWheel.Pages
 
             try
             {
-                List<Product> products = SqlHelper.findProducts();
+                products = SqlHelper.findProducts();
+                fullProducts = new List<Product>(products);
+
+                txtBlockAllCount.Text = fullProducts.Count.ToString();
+                txtBlockResultAmountCount.Text = products.Count.ToString();
+
                 lViewProduct.ItemsSource = products;
+
+                cmbSorting.ItemsSource = sortList;
+                cmbFilter.ItemsSource = filterDiscountList;
             }
             catch (Exception ex) { Console.Error.WriteLine(ex.Message); }
         }
+
         public Client(User user)
         {
             InitializeComponent();
@@ -42,10 +66,94 @@ namespace CarService_SteeringWheel.Pages
             {
                 _currUser = user;
 
-                List<Product> products = SqlHelper.findProducts();
+                products = SqlHelper.findProducts();
+                fullProducts = new List<Product>(products);
+
+                txtBlockAllCount.Text = fullProducts.Count.ToString();
+                txtBlockResultAmountCount.Text = products.Count.ToString();
+
                 lViewProduct.ItemsSource = products;
             }
             catch (Exception ex) { Console.Error.WriteLine(ex.Message); }
+        }
+
+        public void UpdateData()
+        {
+            var _products = fullProducts;
+
+            switch (cmbSorting.SelectedIndex)
+            {
+                case 1:
+                    _products = _products
+                        .OrderBy(p =>
+                            Convert.ToDouble(p.CostWithDiscount)
+                        )
+                        .ToList();
+                    break;
+
+                case 2:
+                    _products = _products
+                        .OrderByDescending(p => 
+                            Convert.ToDouble(p.CostWithDiscount)
+                        )
+                        .ToList();
+                    break;
+
+                default:
+                    break;
+            }
+
+            switch (cmbFilter.SelectedIndex)
+            {
+                case 1:
+                    _products = _products
+                        .Where( x => 
+                            x.ProductDiscountAmount  >= 0 && 
+                            x.ProductDiscountAmount < 10
+                        ).ToList(); 
+                    break;
+
+                case 2:
+                    _products = _products
+                        .Where(x =>
+                            x.ProductDiscountAmount >= 10 &&
+                            x.ProductDiscountAmount < 15
+                        ).ToList();
+                    break;
+
+                case 3:
+                    _products = _products
+                        .Where(x =>
+                            x.ProductDiscountAmount >= 15
+                        ).ToList();
+                    break;
+            }
+
+            _products = _products
+                .Where( x => 
+                    x.ProductName
+                        .ToLower()
+                        .Contains( txtSearch.Text.Trim().ToLower() ) 
+                )
+                .ToList();
+            products = _products;
+            txtBlockResultAmountCount.Text = products.Count.ToString();
+            lViewProduct.ItemsSource = products;
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void cmbSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void cmbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
         }
     }
 }
